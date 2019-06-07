e42f685dce4610a88b76fd168b9f5f8b4c406a24
HD <37189335+HDarko@users.noreply.github.com>
2019-06-06 20:30:01 -0400

    Update BlackJack.cpp

--- BlackJack/SourceFiles/BlackJack.cpp
+++ BlackJack/SourceFiles/BlackJack.cpp
@@ -16,14 +16,13 @@ to allow for spliting hands.
 #include"Hand.h"
 #include"Player.h"
 
-void playBlackJack()
-{
+
+	//Simple Blackjack in steps
 	//Shuffle Deck
 	//Hand out the cards to each
 	//play a round 
-	//next round
-	//next round
-}
+	//next round (Y/N)
+
 
 int main()
 {
@@ -55,13 +54,3 @@ int main()
 
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
